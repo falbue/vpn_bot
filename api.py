e34acb8bdc1c12e26e855fb65684302257ca03e3
Дмитрий <cyansair05@gmail.com>
2026-01-30 16:32:11 +0500

    Добавлена возможность добавлять клиентов

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 import requests
 from TelegramTextApp import config
 from TelegramTextApp.utils.logger import setup as setup_logger
@@ -6,6 +7,27 @@ from TelegramTextApp.utils.logger import setup as setup_logger
 
 logger = setup_logger("API")
 headers = {"Cookie": config.API_TOKEN}
+
+
+def create_client_body(tgid, username="", total_gb=0, expiry_time=0):
+    body = {
+        "clients": [
+            {
+                "id": str(uuid.uuid4()),
+                "flow": "",
+                "email": f"test-{tgid}",
+                "limitIp": 0,
+                "totalGB": total_gb,
+                "expiryTime": expiry_time,
+                "enable": True,
+                "tgId": "",
+                "subId": str(uuid.uuid4()),
+                "comment": username,
+                "reset": 0,
+            }
+        ]
+    }
+    return json.dumps(body)
 
 
 def print_json(data):  # удобный вывод json
@@ -40,3 +62,34 @@ def get_inbound(inbound_id):
         f"Не удалось получить inbound с id {inbound_id}: {response.status_code} - {response.text}"
     )
     return None
+
+
+def get_client(inbound_id, tg_id, placeholder=""):
+    data = get_inbound(inbound_id)
+    clients = data["settings"]
+    clients = json.loads(clients)["clients"]
+    for client in clients:
+        if client["email"] == placeholder + tg_id:
+            return client
+    return None
+
+
+def create_client(inbound_id, tgid, username, total_gb=0, expiry_time=0):
+    inbound_id = inbound_id
+    response = requests.post(
+        f"{config.URL}/inbounds/addClient",
+        headers=headers,
+        data={
+            "id": inbound_id,
+            "settings": create_client_body(tgid, username, total_gb, expiry_time),
+        },
+    )
+    if response.json().get("success") is True:
+        return None
+
+    logger.error(
+        f"Не удалось создать тестового клиента для inbound с id {inbound_id}: {response.json().get('msg')}"
+    )
+    return {
+        "error_message": f"Не удалось добавить клиента. {response.json().get('msg')}"
+    }
--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import json
 import TelegramTextApp
+from TelegramTextApp import config
 import api
+from TelegramTextApp.utils.utils import print_json
 
 
 def get_inbounds(tta):
@@ -31,11 +33,34 @@ def inbound_clients(tta):
     clients = inbound["settings"]["clients"]
     keyboard = {}
     for client in clients:
-        keyboard[f"inbound|{inbound['id']}|client|{client['id']}"] = (
-            f"{client['email']}"
-        )
+        key = f"inbound|{inbound['id']}|{client['id']}"
+        value = f"{client['email']}"
+        keyboard[key] = value
+
     return keyboard
 
 
+def get_inbound_client(tta):
+    inbound = api.get_inbound(tta.inbound_id)
+    if inbound is None:
+        return {"text": "Ошибка при получении inbound'а"}
+    inbound["settings"] = json.loads(inbound["settings"])
+    clients = inbound["settings"]["clients"]
+    for client in clients:
+        if client["id"] == tta.inbound_client_id:
+            return client
+
+
+def get_test_token(tta):
+    inbound_id = config.TEST_INBOUND
+    client = api.get_client(inbound_id, tta.user.telegram_id, placeholder="test_")
+    if client is None:
+        api.create_client(
+            inbound_id, tta.user.telegram_id, tta.user.username, total_gb=1
+        )
+        client = api.get_client(inbound_id, tta.user.telegram_id, placeholder="test_")
+    return {"client": client}
+
+
 if __name__ == "__main__":
-    TelegramTextApp.start()  # type: ignore
+    TelegramTextApp.start()
